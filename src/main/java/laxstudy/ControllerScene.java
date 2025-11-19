2224c18bddc0a6b758a9b9fca077f516a981a616
tainalmeidaa <tainaluise.a@gmail.com>
2025-11-19 12:54:12 -0300

    scenecontroller funcionando

--- src/main/java/laxstudy/ControllerScene.java
+++ src/main/java/laxstudy/ControllerScene.java
@@ -31,6 +31,36 @@ public class ControllerScene {
 		stage.show();
 		
 	}
+	
+	public void switchToSceneProgresso(ActionEvent event) throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("/views/Progresso.fxml"));
+		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		stage.setTitle("LaxStudy - Progresso");
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+		
+	}
+	
+	public void switchToSceneCronograma(ActionEvent event) throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("/views/Cronograma.fxml"));
+		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		stage.setTitle("LaxStudy - Cronograma");
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+		
+	}
+	
+	public void switchToScenePlaylist(ActionEvent event) throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("/views/Playlist.fxml"));
+		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		stage.setTitle("LaxStudy - Playlist");
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+		
+	}
 
 
 }
